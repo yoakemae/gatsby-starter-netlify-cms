423a4a05d9a38a2ed13ea668a6e08b8808c7cc13
yoakemae <ueda@DESKTOP-U8C97AO.localdomain>
2018-11-08 00:16:19 +0900

    ドメイン変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'カメテック',
-    siteUrl: 'https://zen-keller-dac825.netlify.com'
+    siteUrl: 'https://www.kame-tech.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -37,6 +37,12 @@ module.exports = {
             },
           },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-128905421-1",
       },
     },
     {
